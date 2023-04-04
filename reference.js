634f11f8017dbb36264b4e6ff07524d482e31a1f
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-05 03:54:45 +0600

    third commit

---
+++ reference.js
@@ -0,0 +1,21 @@
+const x = 'Mr. X';
+const y = 'Hossain';
+// console.log(z);
+
+// console.log(name);
+// const name = 'Ananta Jalil';
+// let name = 'Ananta Jalil';
+// var name = 'Ananta Jalil';
+
+// console.log(add2(2, 3));
+
+function add (n1, n2){
+    return n1 + n2;
+}
+
+const add2 = (n1, n2) => n1 + n2;
+
+function doSomething (){
+    const sum = 55;
+}
+// console.log(sum);
