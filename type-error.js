c2072bb644a42e2923cd6090c6d5a5ff620d2828
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-05 03:47:21 +0600

    second commit

---
+++ type-error.js
@@ -0,0 +1,12 @@
+const num = 45;
+// num.push = 36;
+
+// let address;
+// console.log(address.home); 
+
+let address = {city: 'bBaria', post: 6430};
+// console.log(address.home.building); 
+console.log(address?.home?.building);
+
+// const a = 'Mr. X';
+// a = 'Mr. Y';
